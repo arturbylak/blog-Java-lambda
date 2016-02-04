a8d82c7e44f493b0011e5333ca557592e1e238bf
Artur Bylak <Artur.Bylak@cybercom.com>
2016-02-04 15:24:33 +0100

    Added stream example

--- src/main/java/com/bylak/lambda/runner/Main.java
+++ src/main/java/com/bylak/lambda/runner/Main.java
@@ -3,7 +3,11 @@ package com.bylak.lambda.runner;
 import com.bylak.lambda.MultiplierProcessor;
 import com.bylak.lambda.Worker;
 import com.bylak.lambda.spec.ValueProcessor;
+import java.util.Collections;
+import java.util.List;
+import java.util.function.Function;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] arg) {
@@ -23,5 +27,37 @@ public class Main {
         final String textFromSupplier = textSupplier.get();
 
         System.out.println("Response is: " + textFromSupplier);
+    }
+
+    static int getEffectivelyFinalValue() {
+        //effectively final value
+        int value = 10;
+        final Supplier<Integer> supplier = () -> value;
+
+        //compilation error
+        //value = 20;
+
+        return supplier.get();
+    }
+
+    static List<String> toUpperCase(final List<String> values) {
+        if(values == null || values.isEmpty()) {
+            return Collections.emptyList();
+        }
+
+        return values.stream().map(value -> value.toUpperCase()).collect(Collectors.toList());
+    }
+
+    static List<String> toUpperCaseWithoutLambda(final List<String> values) {
+        if(values == null || values.isEmpty()) {
+            return Collections.emptyList();
+        }
+
+        return values.stream().map(new Function<String, String>() {
+            @Override
+            public String apply(String value) {
+                return value.toUpperCase();
+            }
+        }).collect(Collectors.toList());
     }
 }
